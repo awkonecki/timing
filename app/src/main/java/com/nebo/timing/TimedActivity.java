14c5f86101462e1da5619c18435a028aa555568e
awkonecki <awkonecki.developer@gmail.com>
2018-09-21 10:46:07 -0700

    [FTR] Firebase Database Communication
    Feature Description
    
    Commit contains the base logic for communicating with the Firebase
    database service API.  At the moment the data being saved is trival
    and constant (just a string).
    
    Modified the UI to provide a button to force sending of data to the
    database to do some basic setup.
    
    One small note is that for receiving data back from the database using
    Firebase, need to have the POJO for the class cast to support setter
    and getter methods to the class attributes in addition to an empty
    class constructor.
    
    Related Issues
    - #9

---
+++ app/src/main/java/com/nebo/timing/TimedActivity.java
@@ -0,0 +1,15 @@
+package com.nebo.timing;
+
+public class TimedActivity {
+    private String mActivityName;
+
+    public TimedActivity() {}
+
+    public TimedActivity(String name) {
+        this.mActivityName = name;
+    }
+
+    public String getActivityName() {
+        return this.mActivityName;
+    }
+}
