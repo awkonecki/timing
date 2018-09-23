cd009d3303a76124ca532d234b3d6c11a043d543
awkonecki <awkonecki.developer@gmail.com>
2018-09-23 12:45:04 -0700

    [FTR] GraphView Addition
    Feature Description
    
    Addition of the `GraphView` library for working with graphs.
    
    Related Issues
    - #15

--- app/src/main/java/com/nebo/timing/MainActivity.java
+++ app/src/main/java/com/nebo/timing/MainActivity.java
@@ -18,6 +18,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Arrays;
 
@@ -130,6 +133,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
         Log.d(TAG, "onCreate");
     }
