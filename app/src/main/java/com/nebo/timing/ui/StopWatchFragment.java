1f8e7aa56eb018c2f2dcb227ae14903973eb1716
awkonecki <awkonecki.developer@gmail.com>
2018-10-01 21:43:34 -0700

    [FTR] Record Time Fragment
    Feature Description
    
    Population of the fragment by the `MainActivity` activity class.  At
    the moment the fragment manager is populating the default fragment which
    has the initial layout of the stop watch into the main activity.
    
    This shows that the UI for the stop watch is being shown and can move to
    populating the fields appropriately.
    
    Related Issues
    - #21

--- app/src/main/java/com/nebo/timing/ui/StopWatchFragment.java
+++ app/src/main/java/com/nebo/timing/ui/StopWatchFragment.java
@@ -1,4 +1,26 @@
 package com.nebo.timing.ui;
 
-public class StopWatchFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nebo.timing.R;
+
+public class StopWatchFragment extends Fragment{
+    private static final String TAG = "StopWatchFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+
+        View view = inflater.inflate(R.layout.fragment_stop_watch, container, false);
+
+        return view; // super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
