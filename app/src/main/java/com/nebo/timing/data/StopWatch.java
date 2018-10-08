3f652deb42e55c2f3e3250f6425ea7a14ed9a63b
awkonecki <awkonecki.developer@gmail.com>
2018-10-08 13:20:19 -0700

    [FTR] StopWatch Documentation
    Feature Description
    
    Adding documentation to the public members of the `StopWatch` object.
    
    Related Issues
    - #22

--- app/src/main/java/com/nebo/timing/data/StopWatch.java
+++ app/src/main/java/com/nebo/timing/data/StopWatch.java
@@ -1,9 +1,27 @@
 package com.nebo.timing.data;
 
 import android.support.annotation.NonNull;
-import android.util.Log;
 
+/**
+ * @class StopWatch
+ * @brief An object that manages the CountDownTimer class as a private member.  The stop watch
+ *        provides a similar API but removes the knowledge of of the current state of the timer
+ *        from the controlling class.  Expected behavior is that the controlling class will use this
+ *        instead of the `CountUpTimer` directly to manage stop watch behaviors.
+ * @member StopWatchState - enum that defines the supported states for the stop watch.
+ * @member StopWatch - constructor
+ * @member stop - forces a cancel on teh underlying CountDownTimer and clears out the total elapsed
+ *                time.
+ * @member play - will start the CountDownTimer object underneath.
+ * @member pause - keeps the current time that has elapsed.
+ * @member getState - obtain the current state of the stopwatch object.
+ * @member StopWatchTickEvents - interface that allows for controlling object to receive callback
+ *                               events from the stopwatch.
+ */
 public class StopWatch {
+    //**********************************************************************************************
+    // PRIVATE CLASS MEMBERS
+    //**********************************************************************************************
     private CountUpTimer mCountUpTimer = null;
     private final long mFutureTime, mIntervalTime;
     private long mMilliSeconds = 0L, mBaseMilliSeconds = 0L;
@@ -24,6 +42,14 @@ public class StopWatch {
         }
     }
 
+    //**********************************************************************************************
+    // PUBLIC CLASS MEMBERS
+    //**********************************************************************************************
+
+    /**
+     * @enum StopWatchState
+     * @brief Enumerate the supported states, and description string via the toString method.
+     */
     public enum StopWatchState {
         PLAYING {
             @Override
@@ -45,6 +71,15 @@ public class StopWatch {
         }
     }
 
+    /**
+     * @func StopWatch
+     * @brief Constructor for the stopwatch class object.
+     * @param futureTime - represents the interval in which the underlying countuptimer cycles on.
+     * @param futureIntervals - represents how frequent w.r.t the interval that the time will be
+     *                          updated.
+     * @param callback - controlling class defined interface that informs the object where to send
+     *                   the current time elapsed on tick events.
+     */
     public StopWatch(long futureTime, long futureIntervals, @NonNull StopWatchTickEvents callback) {
         mFutureTime = futureTime;
         mIntervalTime = futureIntervals;
@@ -52,16 +87,22 @@ public class StopWatch {
         mCountUpTimer = new CountUpTimer(futureTime, futureIntervals, new CountUpTimerInterface());
     }
 
+    /**
+     * @func play
+     * @brief transition to the play state for the StopWatch object.
+     */
     public void play() {
         switch (mState) {
             case PLAYING:
                 mCountUpTimer.stop();
                 mMilliSeconds = 0L;
+                mBaseMilliSeconds = 0L;
                 break;
             case PAUSED:
                 break;
             case STOPPED:
                 mMilliSeconds = 0L;
+                mBaseMilliSeconds = 0L;
                 break;
             default:
                 throw new java.lang.IllegalArgumentException(
@@ -74,12 +115,17 @@ public class StopWatch {
         mState = StopWatchState.PLAYING;
     }
 
+    /**
+     * @func stop
+     * @brief transition to the stop state for the StopWatch object.
+     */
     public void stop() {
         switch (mState) {
             case PLAYING:
             case PAUSED:
             case STOPPED:
                 mMilliSeconds = 0L;
+                mBaseMilliSeconds = 0L;
                 break;
             default:
                 throw new java.lang.IllegalArgumentException(
@@ -91,6 +137,10 @@ public class StopWatch {
         mState = StopWatchState.STOPPED;
     }
 
+    /**
+     * @func pause
+     * @brief transition to the pause state for the StopWatchObject
+     */
     public void pause() {
         switch (mState) {
             case PLAYING:
@@ -107,10 +157,20 @@ public class StopWatch {
         mState = StopWatchState.PAUSED;
     }
 
+    /**
+     * @func getState
+     * @brief Obtain the current state of which the StopWatch objec is in.
+     * @return
+     */
     public StopWatchState getState() {
         return mState;
     }
 
+    /**
+     * @interface StopWatchTickEvents
+     * @brief provides the definition in which the StopWatch object can notify the controlling class
+     *        when events occur.
+     */
     public interface StopWatchTickEvents {
         void tickEvent(long milliSecondsElapsed);
     }
