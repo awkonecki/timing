9d136e05da3047465b6f02703928d7ccb369be8e
awkonecki <awkonecki.developer@gmail.com>
2018-10-10 12:43:02 -0700

    [FTR] StopWatchActivity Fragments Layouts
    Feature Description
    
    Creation of the basic xml widgets that will be responsible for displaying
    the StopWatch activity UI to the user.
    
    Related Issues
    - #31

---
+++ app/src/main/java/com/nebo/timing/StopWatchActivity.java
@@ -0,0 +1,13 @@
+package com.nebo.timing;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class StopWatchActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
