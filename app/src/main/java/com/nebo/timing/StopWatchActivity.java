96a697f36f9dc88679e573b63fe9e92b6b31dbe4
awkonecki <awkonecki.developer@gmail.com>
2018-10-10 13:10:12 -0700

    [FTR] Addition of Menu & Fragment Logic Classes for StopWatchActivity
    Feature Description
    
    Skeleton Fragment logic classes added for the StopWatchActivity which will own
    the fragments via a fragment manager.  Additionally added support for a menu item
    for the save operation that will be responsible for saving the timing information
    for a given activity.
    
    Related Issues
    - #31

--- app/src/main/java/com/nebo/timing/StopWatchActivity.java
+++ app/src/main/java/com/nebo/timing/StopWatchActivity.java
@@ -1,13 +1,31 @@
 package com.nebo.timing;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.nebo.timing.databinding.ActivityStopwatchBinding;
 
 public class StopWatchActivity extends AppCompatActivity {
+    ActivityStopwatchBinding mBinding = null;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_stopwatch);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_stopwatch, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
