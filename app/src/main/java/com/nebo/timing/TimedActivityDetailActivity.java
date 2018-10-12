336394cffe5731ad8e4b62f70390c92e65e85862
awkonecki <awkonecki.developer@gmail.com>
2018-10-11 18:14:37 -0700

    [FTR] Timed Activity Detail RecyclerView
    Feature Description
    
    Setting up the recyclerview widget for the time activity sessions.  Skeleton
    setup that will break out the session into distinct rows within the
    recyclerview widget.
    
    Related Issues
    -#37

--- app/src/main/java/com/nebo/timing/TimedActivityDetailActivity.java
+++ app/src/main/java/com/nebo/timing/TimedActivityDetailActivity.java
@@ -3,8 +3,12 @@ package com.nebo.timing;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.nebo.timing.data.TimedActivity;
 import com.nebo.timing.databinding.ActivityTimedActivityDetailBinding;
@@ -12,6 +16,32 @@ import com.nebo.timing.databinding.ActivityTimedActivityDetailBinding;
 public class TimedActivityDetailActivity extends AppCompatActivity {
     private ActivityTimedActivityDetailBinding mBinding = null;
     private TimedActivity mTimedActivity = null;
+
+    private class ActivitySessionsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        private class ActivitySessionViewHolder extends RecyclerView.ViewHolder {
+
+            public ActivitySessionViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,13 +58,10 @@ public class TimedActivityDetailActivity extends AppCompatActivity {
                     getString(R.string.key_timed_activity));
         }
         else {
-            // use the intent passed data.
+            // use the intent passed data, assume for now the data is passed in via the intent.
             mTimedActivity = getIntent().getParcelableExtra(getString(R.string.key_timed_activity));
         }
 
-        // will likely want the activity to perform the query on the data based on the activity /
-        // user key combination from persistent storage.
 
-        // assume for now the data is populated somewhere.
     }
 }
