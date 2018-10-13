7991cc92da595d31f8cb9d1edf17e28d4bbd6ac5
awkonecki <awkonecki.developer@gmail.com>
2018-10-12 19:50:29 -0700

    [FTR] ActivityTimerActivity UI Initialization
    Feature Description
    
    Handling the population of the recyclerview widget with its resources.  Will
    wait for the handling of the graph information later.
    
    Related Issues
    - #44
    - #45

--- app/src/main/java/com/nebo/timing/ActivityTimerActivity.java
+++ app/src/main/java/com/nebo/timing/ActivityTimerActivity.java
@@ -1,15 +1,44 @@
 package com.nebo.timing;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 
+import com.nebo.timing.data.TimedActivity;
 import com.nebo.timing.databinding.ActivityTimerActivityBinding;
 
+import java.util.ArrayList;
+
 public class ActivityTimerActivity extends AppCompatActivity {
+    private ActivityTimerActivityBinding mBinding = null;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_timer_activity);
+
+        if (savedInstanceState != null) {
+
+        }
+        else {
+            // This is the base activity (main activity) thus do not expect to process data from
+            // intent.
+        }
+
+        // Setup of the UI recyclerview widget
+        mBinding.rvTimedActivities.setAdapter(new TimedActivityAdapter(
+                this,
+                new ArrayList<TimedActivity>()));
+        mBinding.rvTimedActivities.setLayoutManager(new LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false));
+        mBinding.rvTimedActivities.setHasFixedSize(true);
+
+        // Population of the Pie chart based on previously stored data.
     }
 }
