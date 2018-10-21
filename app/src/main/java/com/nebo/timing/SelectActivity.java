1ef4d68caaf66c2c92132ebe59529178222a2b4d
Andrew Konecki <awkonecki.developer@gmail.com>
2018-10-20 18:23:09 -0700

    [FTR] onCreate always Forces off State
    Feature Description
    
    Want to make sure that the UI upon re-call of the activity lifecycle
    that envokes the `onCreate` method will cause the toggle to be set to
    off to force the user to re-select for the time being.
    
    Related Issues
    - #59

--- app/src/main/java/com/nebo/timing/SelectActivity.java
+++ app/src/main/java/com/nebo/timing/SelectActivity.java
@@ -147,6 +147,7 @@ public class SelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_select_activity);
+        mBinding.tbUseNewActivityToggle.setChecked(false);
 
         if (savedInstanceState != null) {
             mActivities = savedInstanceState.getParcelableArrayList(
